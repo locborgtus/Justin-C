b122f3b68a029d93ebee7749a1dbd006a3e2a1b8
locborgtus <locborgtus@gmail.com>
2026-04-12 04:02:10 +0000

    update compound with interest

--- compound/compound.c
+++ compound/compound.c
@@ -1,19 +1,25 @@
 #include <stdio.h> 
 
 int main(void) {
-  double money = 0.01;
+  double money = 100000.0;
   int days = 0;
+  const int goal = 1e6;
+  const double interest = 1.10;
 
   // refactor hard-coded numbers to variables
   // for example: rate as a percentage, target
   // ask the user for 1) the rate percentage 2) the target they want to get to
 
-  while (money <= 1000000000.0) {
-    money *= 2; 
+  while (money <= goal) {
+    money *= interest; 
     days++;
     printf("day %d: $%.2f\n", days + 1, money);
   }
-  printf("It took a total of %d days to reach $1,000,000,000", days + 1);
+
+  printf("Interest rate today is: %.2f% \n", (interest - 1) * 100);
+  printf("It took a total of %d days to reach : $%d \n", days + 1, goal);
+  
   return 0;
 }
+// returns a spons
   
